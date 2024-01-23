40d3436c31bd964e2bbe081bb9c73288fd55f706
Dipak Giri <dipakgiri1200@gmail.com>
2024-01-23 14:24:48 +0545

    displaying criticscore

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '75762b0b5f6c479fbc85a8baa071a893',
+    key: '8c54d28c5cea44589f8295385303f5ae',
   },
 })
